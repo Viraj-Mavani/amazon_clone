9c31ae6e0c87f30c009c7878b12d88eea4e69a44
Viraj-Mavani <66798733+Viraj-Mavani@users.noreply.github.com>
2023-02-23 15:11:06 +0530

    5.17

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -6,6 +6,7 @@ import 'package:amazon_clone/constants/global_variable.dart';
 import 'package:amazon_clone/constants/utils.dart';
 // import 'package:amazon_clone/features/admin/services/admin_services.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dotted_border/dotted_border.dart';
 // import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -59,12 +60,12 @@ class _AddProductScreenState extends State<AddProductScreen> {
   //   }
   // }
 
-  // void selectImages() async {
-  //   var res = await pickImages();
-  //   setState(() {
-  //     images = res;
-  //   });
-  // }
+  void selectImages() async {
+    var res = await pickImages();
+    setState(() {
+      images = res;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,115 +86,116 @@ class _AddProductScreenState extends State<AddProductScreen> {
           ),
         ),
       ),
-      // body: SingleChildScrollView(
-      //   child: Form(
-      //     key: _addProductFormKey,
-      //     child: Padding(
-      //       padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      //       child: Column(
-      //         children: [
-      //           const SizedBox(height: 20),
-      //           images.isNotEmpty
-      //               ? CarouselSlider(
-      //                   items: images.map(
-      //                     (i) {
-      //                       return Builder(
-      //                         builder: (BuildContext context) => Image.file(
-      //                           i,
-      //                           fit: BoxFit.cover,
-      //                           height: 200,
-      //                         ),
-      //                       );
-      //                     },
-      //                   ).toList(),
-      //                   options: CarouselOptions(
-      //                     viewportFraction: 1,
-      //                     height: 200,
-      //                   ),
-      //                 )
-      //               : GestureDetector(
-      //                   onTap: selectImages,
-      //                   child: DottedBorder(
-      //                     borderType: BorderType.RRect,
-      //                     radius: const Radius.circular(10),
-      //                     dashPattern: const [10, 4],
-      //                     strokeCap: StrokeCap.round,
-      //                     child: Container(
-      //                       width: double.infinity,
-      //                       height: 150,
-      //                       decoration: BoxDecoration(
-      //                         borderRadius: BorderRadius.circular(10),
-      //                       ),
-      //                       child: Column(
-      //                         mainAxisAlignment: MainAxisAlignment.center,
-      //                         children: [
-      //                           const Icon(
-      //                             Icons.folder_open,
-      //                             size: 40,
-      //                           ),
-      //                           const SizedBox(height: 15),
-      //                           Text(
-      //                             'Select Product Images',
-      //                             style: TextStyle(
-      //                               fontSize: 15,
-      //                               color: Colors.grey.shade400,
-      //                             ),
-      //                           ),
-      //                         ],
-      //                       ),
-      //                     ),
-      //                   ),
-      //                 ),
-      //           const SizedBox(height: 30),
-      //           CustomTextField(
-      //             controller: productNameController,
-      //             hintText: 'Product Name',
-      //           ),
-      //           const SizedBox(height: 10),
-      //           CustomTextField(
-      //             controller: descriptionController,
-      //             hintText: 'Description',
-      //             maxLines: 7,
-      //           ),
-      //           const SizedBox(height: 10),
-      //           CustomTextField(
-      //             controller: priceController,
-      //             hintText: 'Price',
-      //           ),
-      //           const SizedBox(height: 10),
-      //           CustomTextField(
-      //             controller: quantityController,
-      //             hintText: 'Quantity',
-      //           ),
-      //           const SizedBox(height: 10),
-      //           SizedBox(
-      //             width: double.infinity,
-      //             child: DropdownButton(
-      //               value: category,
-      //               icon: const Icon(Icons.keyboard_arrow_down),
-      //               items: productCategories.map((String item) {
-      //                 return DropdownMenuItem(
-      //                   value: item,
-      //                   child: Text(item),
-      //                 );
-      //               }).toList(),
-      //               onChanged: (String? newVal) {
-      //                 setState(() {
-      //                   category = newVal!;
-      //                 });
-      //               },
-      //             ),
-      //           ),
-      //           const SizedBox(height: 10),
-      //           CustomButton(
-      //             text: 'Sell',
-      //             onTap: sellProduct,
-      //           ),
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _addProductFormKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                images.isNotEmpty
+                    ? CarouselSlider(
+                        items: images.map(
+                          (i) {
+                            return Builder(
+                              builder: (BuildContext context) => Image.file(
+                                i,
+                                fit: BoxFit.cover,
+                                height: 200,
+                              ),
+                            );
+                          },
+                        ).toList(),
+                        options: CarouselOptions(
+                          viewportFraction: 1,
+                          height: 200,
+                        ),
+                      )
+                    : GestureDetector(
+                        onTap: selectImages,
+                        child: DottedBorder(
+                          borderType: BorderType.RRect,
+                          radius: const Radius.circular(10),
+                          dashPattern: const [10, 4],
+                          strokeCap: StrokeCap.round,
+                          child: Container(
+                            width: double.infinity,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.folder_open,
+                                  size: 40,
+                                ),
+                                const SizedBox(height: 15),
+                                Text(
+                                  'Select Product Images',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey.shade400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                const SizedBox(height: 30),
+                CustomTextField(
+                  controller: productNameController,
+                  hintText: 'Product Name',
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: descriptionController,
+                  hintText: 'Description',
+                  maxLines: 7,
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: priceController,
+                  hintText: 'Price',
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: quantityController,
+                  hintText: 'Quantity',
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButton(
+                    value: category,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: productCategories.map((String item) {
+                      return DropdownMenuItem(
+                        value: item,
+                        child: Text(item),
+                      );
+                    }).toList(),
+                    onChanged: (String? newVal) {
+                      setState(() {
+                        category = newVal!;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(height: 10),
+                CustomButton(
+                  text: 'Sell',
+                  onTap: () {},
+                  // onTap: sellProduct,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
