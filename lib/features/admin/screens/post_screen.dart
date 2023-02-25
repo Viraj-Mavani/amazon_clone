73449ec4a75422d88c4530e5a7309ce2b812f1be
Viraj-Mavani <66798733+Viraj-Mavani@users.noreply.github.com>
2023-02-25 14:34:26 +0530

    6.17

--- lib/features/admin/screens/post_screen.dart
+++ lib/features/admin/screens/post_screen.dart
@@ -3,7 +3,11 @@
 // import 'package:amazon_clone/features/admin/screens/add_product_screen.dart';
 // import 'package:amazon_clone/features/admin/services/admin_services.dart';
 // import 'package:amazon_clone/models/product.dart';
+import 'package:amazon_clone/common/widgets/loader.dart';
+import 'package:amazon_clone/features/account/widgets/single_product.dart';
 import 'package:amazon_clone/features/admin/screens/add_product_screen.dart';
+import 'package:amazon_clone/features/admin/services/admin_services.dart';
+import 'package:amazon_clone/models/product.dart';
 import 'package:flutter/material.dart';
 
 class PostsScreen extends StatefulWidget {
@@ -14,30 +18,30 @@ class PostsScreen extends StatefulWidget {
 }
 
 class _PostsScreenState extends State<PostsScreen> {
-  // List<Product>? products;
-  // final AdminServices adminServices = AdminServices();
+  List<Product>? products;
+  final AdminServices adminServices = AdminServices();
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   fetchAllProducts();
-  // }
+  @override
+  void initState() {
+    super.initState();
+    fetchAllProducts();
+  }
 
-  // fetchAllProducts() async {
-  //   products = await adminServices.fetchAllProducts(context);
-  //   setState(() {});
-  // }
+  fetchAllProducts() async {
+    products = await adminServices.fetchAllProducts(context);
+    setState(() {});
+  }
 
-  // void deleteProduct(Product product, int index) {
-  //   adminServices.deleteProduct(
-  //     context: context,
-  //     product: product,
-  //     onSuccess: () {
-  //       products!.removeAt(index);
-  //       setState(() {});
-  //     },
-  //   );
-  // }
+  void deleteProduct(Product product, int index) {
+    adminServices.deleteProduct(
+      context: context,
+      product: product,
+      onSuccess: () {
+        products!.removeAt(index);
+        setState(() {});
+      },
+    );
+  }
 
   void navigateToAddProduct() {
     Navigator.pushNamed(context, AddProductScreen.routeName);
@@ -45,55 +49,52 @@ class _PostsScreenState extends State<PostsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    /**products == null
+    return products == null
         ? const Loader()
-        : **/
-    return Scaffold(
-      body: const Center(
-        child: Text('Product'),
-      ),
-      // body: GridView.builder(
-      //   itemCount: products!.length,
-      //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-      //       crossAxisCount: 2),
-      //   itemBuilder: (context, index) {
-      //     final productData = products![index];
-      //     return Column(
-      //       children: [
-      //         SizedBox(
-      //           height: 140,
-      //           child: SingleProduct(
-      //             image: productData.images[0],
-      //           ),
-      //         ),
-      //         Row(
-      //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //           children: [
-      //             Expanded(
-      //               child: Text(
-      //                 productData.name,
-      //                 overflow: TextOverflow.ellipsis,
-      //                 maxLines: 2,
-      //               ),
-      //             ),
-      //             IconButton(
-      //               onPressed: () => deleteProduct(productData, index),
-      //               icon: const Icon(
-      //                 Icons.delete_outline,
-      //               ),
-      //             ),
-      //           ],
-      //         ),
-      //       ],
-      //     );
-      //   },
-      // ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: navigateToAddProduct,
-        tooltip: 'Add a Product',
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+        : Scaffold(
+            body: GridView.builder(
+              itemCount: products!.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                final productData = products![index];
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 140,
+                      child: SingleProduct(
+                        image: productData.images[0],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            productData.name,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () => deleteProduct(productData, index),
+                          icon: const Icon(
+                            Icons.delete_outline,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: navigateToAddProduct,
+              tooltip: 'Add a Product',
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
   }
 }
--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -67,4 +67,162 @@ class AdminServices {
   }
 
   //get all products
+  Future<List<Product>> fetchAllProducts(BuildContext context) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    List<Product> productList = [];
+    try {
+      http.Response res =
+          await http.get(Uri.parse('$uri/admin/get-products'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': userProvider.user.token,
+      });
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          for (int i = 0; i < jsonDecode(res.body).length; i++) {
+            productList.add(
+              Product.fromJson(
+                jsonEncode(
+                  jsonDecode(res.body)[i],
+                ),
+              ),
+            );
+          }
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return productList;
+  }
+
+  void deleteProduct({
+    required BuildContext context,
+    required Product product,
+    required VoidCallback onSuccess,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+
+    try {
+      http.Response res = await http.post(
+        Uri.parse('$uri/admin/delete-product'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': userProvider.user.token,
+        },
+        body: jsonEncode({
+          'id': product.id,
+        }),
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          onSuccess();
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  // Future<List<Order>> fetchAllOrders(BuildContext context) async {
+  //   final userProvider = Provider.of<UserProvider>(context, listen: false);
+  //   List<Order> orderList = [];
+  //   try {
+  //     http.Response res =
+  //         await http.get(Uri.parse('$uri/admin/get-orders'), headers: {
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //       'x-auth-token': userProvider.user.token,
+  //     });
+
+  //     httpErrorHandle(
+  //       response: res,
+  //       context: context,
+  //       onSuccess: () {
+  //         for (int i = 0; i < jsonDecode(res.body).length; i++) {
+  //           orderList.add(
+  //             Order.fromJson(
+  //               jsonEncode(
+  //                 jsonDecode(res.body)[i],
+  //               ),
+  //             ),
+  //           );
+  //         }
+  //       },
+  //     );
+  //   } catch (e) {
+  //     showSnackBar(context, e.toString());
+  //   }
+  //   return orderList;
+  // }
+
+  // void changeOrderStatus({
+  //   required BuildContext context,
+  //   required int status,
+  //   required Order order,
+  //   required VoidCallback onSuccess,
+  // }) async {
+  //   final userProvider = Provider.of<UserProvider>(context, listen: false);
+
+  //   try {
+  //     http.Response res = await http.post(
+  //       Uri.parse('$uri/admin/change-order-status'),
+  //       headers: {
+  //         'Content-Type': 'application/json; charset=UTF-8',
+  //         'x-auth-token': userProvider.user.token,
+  //       },
+  //       body: jsonEncode({
+  //         'id': order.id,
+  //         'status': status,
+  //       }),
+  //     );
+
+  //     httpErrorHandle(
+  //       response: res,
+  //       context: context,
+  //       onSuccess: onSuccess,
+  //     );
+  //   } catch (e) {
+  //     showSnackBar(context, e.toString());
+  //   }
+  // }
+
+  // Future<Map<String, dynamic>> getEarnings(BuildContext context) async {
+  //   final userProvider = Provider.of<UserProvider>(context, listen: false);
+  //   List<Sales> sales = [];
+  //   int totalEarning = 0;
+  //   try {
+  //     http.Response res =
+  //         await http.get(Uri.parse('$uri/admin/analytics'), headers: {
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //       'x-auth-token': userProvider.user.token,
+  //     });
+
+  //     httpErrorHandle(
+  //       response: res,
+  //       context: context,
+  //       onSuccess: () {
+  //         var response = jsonDecode(res.body);
+  //         totalEarning = response['totalEarnings'];
+  //         sales = [
+  //           Sales('Mobiles', response['mobileEarnings']),
+  //           Sales('Essentials', response['essentialEarnings']),
+  //           Sales('Books', response['booksEarnings']),
+  //           Sales('Appliances', response['applianceEarnings']),
+  //           Sales('Fashion', response['fashionEarnings']),
+  //         ];
+  //       },
+  //     );
+  //   } catch (e) {
+  //     showSnackBar(context, e.toString());
+  //   }
+  //   return {
+  //     'sales': sales,
+  //     'totalEarnings': totalEarning,
+  //   };
+  // }
 }
