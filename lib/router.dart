8a1a1b2e24e1603f8e51f3a9ec3fc0dc72dd093b
Viraj-Mavani <66798733+Viraj-Mavani@users.noreply.github.com>
2023-03-07 15:56:47 +0530

    9.56

--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,12 @@
 import 'package:amazon_clone/common/widgets/bottom_bar.dart';
+import 'package:amazon_clone/features/address/screens/address_screen.dart';
 import 'package:amazon_clone/features/admin/screens/add_product_screen.dart';
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
 import 'package:amazon_clone/features/home/screens/category_deals_screen.dart';
+import 'package:amazon_clone/features/order_details/screens/order_details.dart';
 import 'package:amazon_clone/features/product_details/screens/product_details_screen.dart';
 import 'package:amazon_clone/features/search/screens/search_screen.dart';
+import 'package:amazon_clone/models/order.dart';
 import 'package:amazon_clone/models/product.dart';
 import 'package:flutter/material.dart';
 
@@ -55,22 +58,22 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
           product: product,
         ),
       );
-    // case AddressScreen.routeName:
-    // var totalAmount = routeSettings.arguments as String;
-    // return MaterialPageRoute(
-    //   settings: routeSettings,
-    //   builder: (_) => AddressScreen(
-    //     totalAmount: totalAmount,
-    //   ),
-    // );
-    // case OrderDetailScreen.routeName:
-    // var order = routeSettings.arguments as Order;
-    // return MaterialPageRoute(
-    //   settings: routeSettings,
-    //   builder: (_) => OrderDetailScreen(
-    //     order: order,
-    //   ),
-    // );
+    case AddressScreen.routeName:
+      var totalAmount = routeSettings.arguments as String;
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => AddressScreen(
+          totalAmount: totalAmount,
+        ),
+      );
+    case OrderDetailScreen.routeName:
+      var order = routeSettings.arguments as Order;
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => OrderDetailScreen(
+          order: order,
+        ),
+      );
     default:
       return MaterialPageRoute(
         settings: routeSettings,
